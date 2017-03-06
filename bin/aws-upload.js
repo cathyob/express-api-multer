0bc5ee8509fdc9f02e371cb6dd6adfd5b812aca7
cathyob <cathy.obrien09@gmail.com>
2017-03-06 14:18:14 -0500

    Fixes filename and folder problem
    Previously files were just getting saved in the root directory of our bucket. Solved by adding today's date as a folder name and prepending our filename with that. The next thing we did via crypto.randomBytes is create a random string to save our file as on AWS. The last thing we did was promisify both s3.upload and crypto.randomBytes so both implement promises that are resolved or rejected based on whether or not they work.

--- bin/aws-upload.js
+++ bin/aws-upload.js
@@ -7,6 +7,7 @@ const s3 = new AWS.S3();
 const fs = require('fs');
 const path = require('path');
 const mime = require('mime');
+const crypto = require('crypto');
 
 // process.argv is how you access scripts in terminal
 let file = {
@@ -17,18 +18,60 @@ let file = {
 let mimeType = mime.lookup(file.path);
 let ext = path.extname(file.path);
 
+// get the date, changes date to string, splits up-to time, and only give the first part before time
+let folder = (new Date()).toISOString().split('T')[0];
 let stream = fs.createReadStream(file.path);
 
-console.log("stream is ", stream);
+new Promise((resolve, reject) => {
+  // randomBytes takes a callback but we want it to be a Promise
+  crypto.randomBytes(16, (error, buffer) =>{
+    // if an error skip everything by rejecting
+    if(error){
+      reject(error);
+    }
+    // otherwise, if no error change the buffer to a string
+    else {
+      console.log("buffer is ", buffer);
+      console.log("buffer.toS is ", buffer.toString('hex'));
+      resolve(buffer.toString('hex'));
+    }
+  });
+// if no error, then do this...
+}).then((filename)=>{
+  // create the file name with the info that has been provided
+  let params = {
+    ACL: 'public-read',
+    ContentType: mimeType,
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: `${folder}/${filename}${ext}`,
+    Body: stream
+  };
 
-let params = {
-  ACL: 'public-read',
-  ContentType: mimeType,
-  Bucket: process.env.AWS_S3_BUCKET_NAME,
-  Key: `${file.title}${ext}`,
-  Body: stream
-};
-
-s3.upload(params, function (err, data) {
-  console.log(err, data);
+  // make a new promise
+  // promise version
+  return new Promise((resolve, reject) => {
+    // call s3.upload
+    s3.upload(params, function (error, data) {
+      if(error){
+        // if an error occurs here skip everything else
+        // console.log(error);
+        reject(error);
+      }
+      else {
+        // otherwise, if no error resolve the data
+        // console.log(data);
+        resolve(data);
+      }
+    });
+  })
+  // if promise is resolved do this
+  .then(console.log)
+  // if promise has an error, catch it and console.log it
+  .catch(console.log);
 });
+
+
+// non-promise version of line
+// s3.upload(params, function (err, data) {
+//   console.log(err, data);
+// });
